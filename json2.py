016f384ea31a39ba9618b883ef16b132b4690a2f
Joshua Hockerman <jhockerman96@gmail.com>
2021-10-06 15:03:43 -0500

    Commit After class, need to update JSON1 still

--- json2.py
+++ json2.py
@@ -2,7 +2,6 @@ from plotly import offline
 from plotly.graph_objs import Scattergeo, Layout
 import json
 
-
 infile = open("eq_data_1_day_m1.json", "r")
 outfile = open("readable_eq_data.json", "w")
 
@@ -19,17 +18,33 @@ hover_text = []
 
 for n in range(len(eq_data["features"])):
     magnitude.append(eq_data["features"][n]["properties"]["mag"])
-    latitude.append(eq_data["features"][n]["geometry"]["coordinates"][0])
-    longitude.append(eq_data["features"][n]["geometry"]["coordinates"][1])
+    longitude.append(eq_data["features"][n]["geometry"]["coordinates"][0])
+    latitude.append(eq_data["features"][n]["geometry"]["coordinates"][1])
+    hover_text.append(eq_data["features"][n]["properties"]["title"])
 
 print(magnitude[:5])
 print(latitude[:5])
 print(longitude[:5])
+print(hover_text[:5])
 
-data = [Scattergeo(lon=lons)]
+data = [
+    {
+        "type": "scattergeo",
+        "lon": longitude,
+        "lat": latitude,
+        "text": hover_text,
+        "marker": {
+            "size": [5 * i for i in magnitude],
+            "color": magnitude,
+            "colorscale": "Viridis",
+            "reversescale": True,
+            "colorbar": {"title": "Magnitude"},
+        },
+    }
+]
 
 my_layout = Layout(title="Global Earthquake 1 Day")
 
 fig = {"data": data, "layout": my_layout}
 
-offline.plot(fig, filenam="globalearthquake1day.html")
+offline.plot(fig, filename="globalearthquake1day.html")
